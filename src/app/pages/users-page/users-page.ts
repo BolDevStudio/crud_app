035244d10d5a9c81fe9c6812ccee74428b79568c
BolDevStudio <bolisega132@gmail.com>
2025-08-13 11:43:17 +0200

    Add user

--- src/app/pages/users-page/users-page.ts
+++ src/app/pages/users-page/users-page.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {User} from '../../models/user.model';
+import {UserService} from '../../services/user-service';
 
 @Component({
   selector: 'app-users-page',
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './users-page.html',
   styleUrl: './users-page.css'
 })
 export class UsersPage {
-
+newUser: User = {id:0, name:'', email:''};
+users: User[] = [];
+constructor(private userService: UserService) {
+  this.users = this.userService.getUsers();
+}
+addUser() {
+  if (this.newUser.name && this.newUser.email) {
+    this.userService.addUser({ ...this.newUser});
+    this.newUser = {id: 0, name: '', email: ''};
+  }
+}
 }
